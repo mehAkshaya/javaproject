b6e69cf93e2050aa98c321b728b60098a7f4171b
mehAkshaya <akshayaverma184@gmail.com>
2025-09-25 23:22:11 +0530

    Update Main.java

--- java_project-main/src/edu/ccrm/Main.java
+++ java_project-main/src/edu/ccrm/Main.java
@@ -2,13 +2,19 @@ package edu.ccrm;
 
 import edu.ccrm.cli.MenuHandler;
 
-/**
- * The main starting point for the CCRM application.
- */
-public class Main {
+public class MainApp {
+
     public static void main(String[] args) {
-        System.out.println("Starting Campus Course & Records Manager...");
-        MenuHandler menu = new MenuHandler();
-        menu.run();
+        System.out.println("=== Welcome to the CCRM System ===");
+
+        
+        if (args.length > 0) {
+            System.out.println("Note: Command-line arguments detected (" + args.length + ")");
+        }
+
+        
+        new MenuHandler().run();
+
+        System.out.println("Thank you for using CCRM. Goodbye!");
     }
-}               
+}
